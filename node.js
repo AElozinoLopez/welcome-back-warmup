b47a2377ff23540fd78f1f9e06c9a9de7b7fee04
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-06-14 07:23:33 +0100

    Corrected a syntax error

--- node.js
+++ node.js
@@ -1,10 +1,10 @@
 console.log('Please enter a number');
-ProcessingInstruction.stdin.on('data', (data) => {
+process.stdin.on('data', (data) => {
     const num = parseInt(data.toString())
     if (num % 2 === 0) {
         precess.stdout.write(num.toString() + "is even")
     }
     else {
-        ProcessingInstruction.stdout.write  (num.toString() + "is odd")
+        process.stdout.write  (num.toString() + "is odd")
     }
 })
