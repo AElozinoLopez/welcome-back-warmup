248d5b3b1ad70a51aad48194d87fe915fc89620a
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-06-14 07:29:37 +0100

    Corrected typo

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ console.log('Please enter a number');
 process.stdin.on('data', (data) => {
     const num = parseInt(data.toString())
     if (num % 2 === 0) {
-        precess.stdout.write(num.toString() + "is even")
+        process.stdout.write(num.toString() + "is even")
     }
     else {
         process.stdout.write  (num.toString() + "is odd")
