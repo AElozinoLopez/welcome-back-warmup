c1c94a6ed52319037570367d9031255721112735
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-06-15 08:05:41 +0100

    Created a new app.js file

--- node.js
+++ node.js
@@ -1,14 +1,14 @@
 // Example using stdin and stdout
-console.log('Please enter a number');
-process.stdin.on('data', (data) => {
-    const num = parseInt(data.toString())
-    if (num % 2 === 0) {
-        process.stdout.write(num.toString() + "is even")
-    }
-    else {
-        process.stdout.write  (num.toString() + "is odd")
-    }
-})
+// console.log('Please enter a number');
+// process.stdin.on('data', (data) => {
+//     const num = parseInt(data.toString())
+//     if (num % 2 === 0) {
+//         process.stdout.write(num.toString() + "is even")
+//     }
+//     else {
+//         process.stdout.write  (num.toString() + "is odd")
+//     }
+// })
 
 process.on('beforeExit', () => {
     console.log('I am here before exit');
