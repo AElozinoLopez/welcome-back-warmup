8f003b7c03aeb5fe2d771c728e8362b68228340e
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-06-19 08:01:13 +0100

    Created a get request

--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,8 @@ const express = require('express');
 
 // Creating the router app
 const router = express.Router();
+
+// CRUD
+ router.get ('/', (req, res) => {
+    res.status(200).send('Hello OSCArians!');
+ })
