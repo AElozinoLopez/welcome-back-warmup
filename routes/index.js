3d27edfdc01f7d226f7a2640d09daae47e9c3037
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-06-19 08:04:27 +0100

    Exported the router app

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 // CRUD
+// GET
  router.get ('/', (req, res) => {
     res.status(200).send('Hello OSCArians!');
  })
+
+// Exporting File Out For Use In Other Locations
+module.exports = {
+    router
+}
