a6b7dc0fae90c446a421fd7ed309392aa7d95cec
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-06-20 06:38:13 +0100

    Set the global route to be at the top

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ app.use(router);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// Global Route
+app.get ('/', (req, res) => {
+    res.send ('Hello World, I am back!')
+})
+
+
 app.get("/secret", (req, res) => {
     res.status(401).json({message: "unauthorised"})
 });
 
-app.get ('/', (req, res) => {
-    res.send ('Hello World, I am back!')
-})
 
 app.listen (port, () => {
     console.log(`Welcome back app is listening on port ${port}`)
