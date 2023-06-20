d0e1201c67ce2a827ea3cb2f1a4ea83fea5be206
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-06-20 06:55:06 +0100

    Created the route to get a course by id

--- app.js
+++ app.js
@@ -14,10 +14,16 @@ app.get ('/', (req, res) => {
     res.send ('Hello World, I am back!')
 })
 
-
+// Route for all the courses
 app.get ('/api/courses', (req, res) => {
     res.send([1, 2, 3, 4]);
 })
+
+// Route to get one course
+app.get('/api/courses/:id', (req, res) => {
+    res.send (req.params.id);
+})
+
 
 app.get("/secret", (req, res) => {
     res.status(401).json({message: "unauthorised"})
