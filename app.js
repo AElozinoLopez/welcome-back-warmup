bcb21f7d977ede5ff999c7acc7d2dd609c5a8ae9
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-06-20 07:15:35 +0100

    Created a list of courses

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const {router} = require ('./routes');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; 
 
 // Middleware
 app.use(router);
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true})); 
+
+const courses = [
+    {id: 1, name: 'JavaScript'},
+    {id: 1, name: 'MongoDB'},
+    {id: 1, name: 'MySQL'},
+    {id: 1, name: 'Node'}
+
+]
 
 // Global Route
 app.get ('/', (req, res) => {
