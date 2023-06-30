d1c81d9cf5ac189c8a4e6e7b3241cec97b4ed2bb
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-06-30 07:28:16 +0100

    Passed a callback  into the async function

--- async-demo/index.js
+++ async-demo/index.js
@@ -7,3 +7,15 @@ function getUser(id) {
         console.log('Thank you for visiting techSpire!', id);
     }, 2000)
 }
+
+// Using A Callback to access an object in an asynchronous code
+console.log("Before");
+getUser('Splendour');
+console.log("After");
+
+function getUser(id) {
+    setTimeout(() => {
+        console.log('Thank you for visiting techSpire!', id);
+        callback({id: 1, gitHubUserName: "babySplendour"});
+    }, 2000)
+}
