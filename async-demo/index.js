6e836fb3983980c103aae19c1482e1caeb6bb961
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-06-30 07:48:10 +0100

    Commented out codes causing callback is not a function bugs

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,23 +1,24 @@
-console.log("Before");
-getUser('Splendour');
-console.log("After");
+// console.log("Before");
+// getUser('Splendour');
+// console.log("After");
 
-function getUser(id) {
-    setTimeout(() => {
-        console.log('Thank you for visiting techSpire!', id);
-    }, 2000)
-}
+// function getUser(id) {
+//     setTimeout(() => {
+//         console.log('Thank you for visiting techSpire!', id);
+//     }, 2000)
+// }
 
 // Using A Callback to access an object in an asynchronous code
 console.log("Before");
-getUser('Splendour', function(id){
-    console.log(id)
+getUser(1, function(user){
+    console.log(user)
 });
+
 console.log("After");
 
 function getUser(id, callback) {
     setTimeout(() => {
         console.log('Thank you for visiting techSpire!');
         callback({id: id, gitHubUserName: "babySplendour"});
-    }, 2000)
+    }, 2000);
 }
