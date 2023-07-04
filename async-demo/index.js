5548edbd02728469334df9dd616be2aa77b9de8a
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-07-04 07:00:50 +0100

    Added a timer to the async function

--- async-demo/index.js
+++ async-demo/index.js
@@ -27,9 +27,9 @@ function getUser(id, callback) {
 }
 
 function getRepositories(user, callback) {
-    setTimeout () {
+    setTimeout (() {
         console.log("Calling GitHub API...");
         callback (["Repo1", "Repo2", "Repo3"]);
-    }
+    }, 2000)
     
 }
