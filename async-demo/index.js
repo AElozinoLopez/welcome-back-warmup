0f5d6dd28fb9bdbdca8da5aed7a521f774ad8605
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-07-05 03:52:57 +0100

    Pasted the callback function to try out name function on it

--- async-demo/index.js
+++ async-demo/index.js
@@ -9,6 +9,42 @@
 // }
 
 // Using A Callback to access an object in an asynchronous code
+// console.log("Before");
+// getUser(1, function(user){
+//     console.log('User..', user)
+//     getRepositories (user.gitHubUserName, (repos) => {
+//         console.log('Repos..', repos);
+//         getCommits (repos, (commits) => {
+//             console.log('Commits..', commits);
+//         });
+//     });
+// });
+
+// console.log("After");
+
+// function getUser(id, callback) {
+//     setTimeout(() => {
+//         console.log('Thank you for visiting techSpire!');
+//         callback({id: id, gitHubUserName: "babySplendour"});
+//     }, 2000);
+// }
+
+// function getRepositories(user, callback) {
+//     setTimeout (() => {
+//         console.log("Calling GitHub API...");
+//         callback (["Repo1", "Repo2", "Repo3"]);
+//     }, 2000)
+    
+// }
+
+// function getCommits (repo, callback) {
+//     setTimeout (()=> {
+//         console.log('Getting user commits..');
+//         callback(['commit']);
+//     }, 2000)
+// }  
+
+// Using  name function to simplify the callback hell
 console.log("Before");
 getUser(1, function(user){
     console.log('User..', user)
