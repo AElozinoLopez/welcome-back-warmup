f44dc5980165e776e346ddc329e9c0675b2abb4d
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-07-08 03:47:07 +0100

    Created a function to test the state of the error

--- async-demo/promise.js
+++ async-demo/promise.js
@@ -1,6 +1,12 @@
 const promise = new Promise ((resolve, reject) => {
     // resolve(1);
-    reject(new Error('Fail!'));  //don't pass a string when handling an object... use am error object
+    // reject(new Error('Fail!'));  //don't pass a string when handling an object... use am error object
+    let x = 5
+    if (x < 5) {
+        resolve('Success');
+    } else {
+        reject(new Error('Error message'));
+    }
 });
 
 // Consuming the promise
