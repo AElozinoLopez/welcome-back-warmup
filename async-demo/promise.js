8b0c93bcf50f26bf2567dc79e28db45baf524c8d
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-07-08 03:31:59 +0100

    Modidied the semantics of the error message

--- async-demo/promise.js
+++ async-demo/promise.js
@@ -1,9 +1,9 @@
 const promise = new Promise ((resolve, reject) => {
-    resolve(1);
+    // resolve(1);
     reject(new Error('Fail!'));  //don't pass a string when handling an object... use am error object
 });
 
 // Consuming the promise
 promise.then(result => console.log(result))
-       .catch(err => console.log(err.message));
+       .catch(err => console.log('Error', err.message));
 // console.log(promise);
