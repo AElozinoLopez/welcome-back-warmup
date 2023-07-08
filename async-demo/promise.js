0e67fa9260e0027dc15e375c6832a6918cb11d5f
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-07-08 03:30:10 +0100

    Passed in an error object

--- async-demo/promise.js
+++ async-demo/promise.js
@@ -1,8 +1,9 @@
 const promise = new Promise ((resolve, reject) => {
     resolve(1);
-    // reject('Message')
+    reject(new Error('Fail!'));  //don't pass a string when handling an object... use am error object
 });
 
 // Consuming the promise
-promise.then(result => console.log(result));
+promise.then(result => console.log(result))
+       .catch(err => console.log(err.message));
 // console.log(promise);
