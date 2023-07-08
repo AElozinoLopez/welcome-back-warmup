5bb34aaf967b24ab1c0cf999ee3253b5e085ea80
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-07-08 05:34:37 +0100

    Commented out the code and renamed the promise.js file

---
+++ async-demo/promises.js
@@ -0,0 +1,15 @@
+// const promise = new Promise ((resolve, reject) => {
+//     // resolve(1);
+//     // reject(new Error('Fail!'));  //don't pass a string when handling an object... use am error object
+//     let x = 4
+//     if (x < 5) {
+//         resolve('Success');
+//     } else {
+//         reject(new Error('Error message'));
+//     }
+// });
+
+// // Consuming the promise
+// promise.then(result => console.log(result))
+//        .catch(err => console.log('Error', err.message));
+// // console.log(promise);
